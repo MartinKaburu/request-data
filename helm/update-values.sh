ee3faef4c3569b5f56be042521508153497fb4c8
martinkaburu <mkaburu@worldremit.com>
2024-02-19 13:57:57 +0300

    Configure helm templates

--- helm/update-values.sh
+++ helm/update-values.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Retrieve the latest Git SHA
-GIT_SHA=$(git rev-parse --short HEAD)
+GIT_SHA=$(git rev-parse HEAD)
 
 # Update values.yaml with the new Git SHA
 sed -i "s/^  tag: .*$/  tag: \"$GIT_SHA\"/" values.yaml
